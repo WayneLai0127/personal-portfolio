3a673bc0218c5cf0da1ef439ce58ccc60e97ff18
Wayne <fssher107@gmail.com>
2023-11-05 21:34:00 +0800

    feat: update basic info

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Wayne Lai</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/WayneLai0127/personal-portfolio',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/WayneLai0127/personal-portfolio',
   footer: {
-    text: 'Nextra Docs Template',
+    text: `Wayne's personal portfolio site powered by Nextra`,
   },
 }
 
